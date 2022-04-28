e86e135dded8f666a1472f3d485bb76dbd04f483
ywon0925 <9788667@gmail.com>
2022-04-28 17:00:22 +0900

    oracle contract complete

--- caller/contracts/EthPriceOracleInterface.sol
+++ caller/contracts/EthPriceOracleInterface.sol
@@ -2,5 +2,5 @@
 pragma solidity >=0.8.0;
 
 interface EthPriceOracleInterface {
-function getLatestEthPrice() external returns (uint256);
+    function getLatestEthPrice() external returns (uint256);
 }
