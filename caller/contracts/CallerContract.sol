2f6ee5b3bf85b6a3391aea7be464ba831ce13472
ywon0925 <9788667@gmail.com>
2022-04-28 15:55:53 +0900

    caller contract complelete

--- caller/contracts/CallerContract.sol
+++ caller/contracts/CallerContract.sol
@@ -6,12 +6,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /// @title Contract to interact w/ Oracle Contract
 /// @author Yuseok Michael Won
 contract CallerContract {
+    uint256 private ethPrice;
     EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
     mapping(uint256 => bool) myRequests;
 
     event newOracleAddressEvent(address oracleAddress);
     event ReceivedNewRequestIdEvent(uint256 id);
+    event PriceUpdatedEvent(uint256 ethPrice, uint256 id);
 
     function setOracleInstanceAddress(address _oracleInstanceAddress) public {
         oracleAddress = _oracleInstanceAddress;
@@ -19,9 +21,20 @@ contract CallerContract {
         emit newOracleAddressEvent(oracleAddress);
     }
 
-    function updateEthPrice() public{
+    function updateEthPrice() public {
         uint256 id = oracleInstance.getLatestEthPrice();
         myRequests[id] = true;
         emit ReceivedNewRequestIdEvent(id);
+    }
+
+    function callback(uint256 _ethPrice, uint256 _id) public onlyOracle {
+        require(myRequests[_id], "This request is not in my pending list.");
+        ethPrice = _ethPrice;
+        delete myRequests[_id];
+        emit PriceUpdatedEvent(_ethPrice, _id);
+    }
+    modifier onlyOracle() {
+        require(msg.sender == oracleAddress,"You are not authorized to call this function.");
+        _;
     }
 }
