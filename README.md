e2fd8a104aa298e874c36037dc57811702c2e6b4
ywon0925 <9788667@gmail.com>
2022-05-04 17:52:42 +0900

    making oracle more decentralized by using roles (accessContro) instead of using Ownable

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # EthPriceOracle
 
-Studying how Oracle works. **Enter**
------
-First, followed the tutorial on [Crypto Zombies](https://cryptozombies.io/en/lesson/14).**Enter**
+Studying how Oracle works.
+
+First, followed the tutorial on [Crypto Zombies](https://cryptozombies.io/en/lesson/14).
+
 Updated the code to use the recent version of Solidity & deployed the contract on Rinkeby (Ethereum Testnet) and tested the Oracle. 
--- oracle/contracts/EthPriceOracle.sol
+++ oracle/contracts/EthPriceOracle.sol
@@ -2,11 +2,15 @@
 pragma solidity >=0.8.0;
 
 import "./ICallerContract.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title Contract that interact w/ Node server
 /// @author Yuseok Michael Won
-contract EthPriceOracle is Ownable {
+contract EthPriceOracle is AccessControl {
+  bytes32 public constant owners = keccak256("owners");
+  bytes32 public constant oracles = keccak256("oracles");
+
   uint private randNonce = 0;
   uint private modulus = 1000;
   mapping(uint256=>bool) pendingRequests;
@@ -14,6 +18,16 @@ contract EthPriceOracle is Ownable {
   event GetLatestEthPriceEvent(address callerAddress, uint id);
   event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
   
+  constructor(address _owner) {
+    grantRole(owners, _owner);
+  }
+
+  function addOracle(address _oracle) public{
+    require(hasRole(owners, msg.sender));
+    require(!hasRole(oracles, _oracle));
+    grantRole(oracles, _oracle);
+  }
+
   /// @notice add randomly generated request id to pending list & return the id
   function getLatestEthPrice() public returns (uint256) {
       randNonce++;
@@ -24,7 +38,7 @@ contract EthPriceOracle is Ownable {
   }
 
   /// @notice if request is valid, remove request from pendding list & callback the _ethPrice to caller contract
-  function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyOwner{
+  function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyRole(owners) {
       require(pendingRequests[_id], "This request is not in my pending list.");
       delete pendingRequests[_id];
       ICallerContract callerContractInstance;
